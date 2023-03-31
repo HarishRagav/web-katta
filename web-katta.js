7c4a28dc5ed5b30f7f2da9725ecfe28da02c0f27
HarishRagav <22mca22@MCASERV.COM>
2023-03-31 19:01:15 +0530

    challange no-2

--- web-katta.js
+++ web-katta.js
@@ -1,2 +1,11 @@
 // Change the color of the text "This is an Umbrella" to blue color by using javascript
 document.getElementById("sid").style.color="blue";
+
+//Display the length of input field value with id 'myLength' in paragraph tag with id 'sid' using JS. Note: Values are assigned dynamically during validation.
+function calculateLength(){
+    //Write your code here
+    const input = document.getElementById("myLength");
+    const sid = document.getElementById("sid");
+    const length = input.value.length;
+    sid.textContent=length;
+}
