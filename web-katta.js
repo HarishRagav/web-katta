981e134a7be83fa9d9864ec6f7604597ca41091d
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-03 12:50:20 +0530

    Challenge no-3

--- web-katta.js
+++ web-katta.js
@@ -9,3 +9,14 @@ function calculateLength(){
     const length = input.value.length;
     sid.textContent=length;
 }
+
+//Add values in Input 1 and input 2 fields with id 'in1', 'in2' and Display the resultant in the paragraph tag with id "result"
+//Note: Values are assigned dynamically for evaluation
+function calculateSum(){
+    //Start Your code here
+    var inp1=document.getElementById("in1").value;
+    var inp2=document.getElementById("in2").value;
+    var calc= Number(inp1) + Number(inp2);
+    var result = document.getElementById("result");
+    result.textContent=calc;
+}
