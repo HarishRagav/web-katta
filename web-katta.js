c2b7a11b43b1c99987299bdf3d0b18d79b5f2541
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-03 23:59:03 +0530

    challenge no-9

--- web-katta.js
+++ web-katta.js
@@ -45,3 +45,12 @@ para.innerText=grab;
     
 }
 getSelectedOption();
+
+// Getting Value Of Radio Button
+//Display the value of selected radio button in paragraph tag with id 'selectedOption' using Javascript. Note: Values are changed dynamically for evaluation
+function displaySelectedOption(){
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+  const selectedOptionText = selectedOption ? selectedOption.value : '';
+  document.getElementById('selectedOption').textContent = selectedOptionText;
+}
+displaySelectedOption();
