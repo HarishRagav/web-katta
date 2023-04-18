3a239f7d186d53452e9c345e89d5041964a24d86
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-18 18:00:59 +0530

    challenge no-17

--- JSchallenger.js
+++ JSchallenger.js
@@ -86,3 +86,13 @@ const numOne = 1;
 const numTwo = 1;
 const numThree = 2;
 console.log(numOne <= numTwo, numTwo <= numThree);
+
+// In this scenario, the existing code adds an eventListener for a click event on a variable buttonElem. It expects buttonElem to be the button element in the example UI. But, that element is not selected yet.
+// Assign the button element to the variable buttonElem. Click the button to verify that the code is working.
+// Hint: Make use of the unique id of the button element.
+  const buttonElem = document.getElementById("button");
+  
+  buttonElem.addEventListener('click', () => {
+    const oldText = buttonElem.innerText;
+    return button.innerText = oldText === "ON" ? "OFF" : "ON";
+  });
