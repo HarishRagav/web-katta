b2b69f21a822613b93ee240b437d30f126c991b0
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-18 17:30:44 +0530

    challenge no-2

--- JSchallenger.js
+++ JSchallenger.js
@@ -2,3 +2,8 @@
 let num = 1;
 num = 2;
 console.log(num);
+
+// Here, we declare the variable num. But, it has no value yet. Assign a value to it and run the code.
+let num;
+num = 2;
+console.log(num);
