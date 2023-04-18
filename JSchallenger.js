92cf5c8b62ccdd869dc753017afa7c2a6078a772
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-18 17:50:49 +0530

    challenge no-12

--- JSchallenger.js
+++ JSchallenger.js
@@ -51,3 +51,9 @@ let num = 5;
 num = 0;
 const bool = Boolean(bool);
 console.log(bool);
+
+// In the console.log() 
+// statement below we use the Equal operator to check whether numOne and numTwo have the same value. Change the code so that the console.log() statement logs true.
+const numOne = 5;
+const numTwo = 5;
+console.log(numOne == numTwo);
