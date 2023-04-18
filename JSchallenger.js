9dcd23b7eaee5fc1ce09d5d0d938052ae9553bb3
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-18 17:48:58 +0530

    challenge no-11

--- JSchallenger.js
+++ JSchallenger.js
@@ -44,3 +44,10 @@ console.log('The value of num is: ' + num);
 let isTrue;
 isTrue = true;
 console.log(isTrue);
+
+// Here, we declare the variable num and assign it the value 5. We also declare the variable bool which we assign the boolean representation of num.
+// Extend the code such that the console.log() statement logs false.
+let num = 5;
+num = 0;
+const bool = Boolean(bool);
+console.log(bool);
