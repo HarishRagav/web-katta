6250e4877ce0f3930f7df9f6610110a99cb71223
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-18 17:54:28 +0530

    challenge no-14

--- JSchallenger.js
+++ JSchallenger.js
@@ -68,3 +68,9 @@ console.log(numOne != numTwo);
 const numOne = 6;
 const numTwo = 5;
 console.log(numOne > numTwo);
+
+// In the console.log() statement below we use the Less Than operator to check whether the value of numOne is less than the value of numTwo. Change the code so that the console.log() 
+// statement logs true.
+const numOne = 1;
+const numTwo = 2;
+console.log(numOne < numTwo);
