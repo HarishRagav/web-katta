96ba0af5e3166f2dff383b75da4999a64656fd7c
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-18 17:42:01 +0530

    challenge no-8

--- JSchallenger.js
+++ JSchallenger.js
@@ -28,3 +28,8 @@ console.log(num);
 // Fix the code and run it.
 const num = 5;
 console.log(num);
+
+// In this exercise we practice how to declare a new variable and how to assign it a number. The console.log() statement below attempts to log a variable named num.
+// Declare a variable with this name and assign it a number of your choice. Run the code to see if the number is being logged.
+const num = 5;
+console.log('The value of num is: ' + num);
