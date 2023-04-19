8ed296552466e4c3bc2d152aa0503053e34d6adb
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-19 23:26:02 +0530

    challenge no-20

--- JSchallenger.js
+++ JSchallenger.js
@@ -7,6 +7,11 @@ function myFunction(a, b) {
   return a === b;
 }
 
+// Write a function that takes a value as argument. Return the type of the value.
+function myFunction(a) {
+   return typeof a;
+}
+
 // Assign a new value to the variable num. The code will not work the way it is. Find the mistake and fix it. Execute the corrected code.
 let num = 1;
 num = 2;
