0a12a08ad88b9c5a612d5933db17d00b8fb07b35
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-21 17:46:09 +0530

    challenge no-29

--- JSchallenger.js
+++ JSchallenger.js
@@ -1,3 +1,8 @@
+// Write a function that takes 6 values (a,b,c,d,e,f) as arguments. Sum a and b. Then substract by c. 
+// Then multiply by d and divide by e. Finally raise to the power of f and return the result. Tipp: mind the order.
+function myFunction(a, b, c, d, e, f) {
+   return (((a + b - c) * d) / e) ** f;
+}
 // return the percentage of the number
 function myFunction(a, b) {
   return b / 100 * a
