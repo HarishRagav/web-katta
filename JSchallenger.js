36a824bdebe1f52a52313b058114cadd9e674c90
Harish Ragav <80840163+HarishRagav@users.noreply.github.com>
2023-04-22 01:20:15 +0530

    challenge no-30

--- JSchallenger.js
+++ JSchallenger.js
@@ -1,5 +1,10 @@
-// Write a function that takes 6 values (a,b,c,d,e,f) as arguments. Sum a and b. Then substract by c. 
-// Then multiply by d and divide by e. Finally raise to the power of f and return the result. Tipp: mind the order.
+// Write a function that takes a string as argument. The string contains the substring 'is'. Return the index of 'is'.
+function myFunction(a){
+   return a.indexOf('is');
+}
+
+// Write a function that takes 6 values (a,b,c,d,e,f) as arguments. Sum a and b. Then substract by c.Then multiply by d and divide by e. 
+//Finally raise to the power of f and return the result. Tipp: mind the order.
 function myFunction(a, b, c, d, e, f) {
    return (((a + b - c) * d) / e) ** f;
 }
